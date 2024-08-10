eabba645c6cd00e7fb415ad076a0a8890b0a3124
Cenz0Red <159074780+Cenz0Red@users.noreply.github.com>
2024-08-10 22:54:52 +0300

    Create MyCocktailsActivity.kt

---
+++ MyCocktailsActivity.kt
@@ -0,0 +1,20 @@
+package com.example.cock_bar
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MyCocktailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_my_cocktails)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
